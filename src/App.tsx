e7311fa6b4dac1150bb3ee5d2f11e29576af8189
Mohamed Badr <mbadr@sequel.ae>
2023-02-21 16:37:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AdminLanding from "./pages/AdminLanding";
+
+function App() {
+  return (
+    <div className="App">
+      <AdminLanding />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AdminLanding from "./pages/AdminLanding";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AdminLanding />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "team",
+    //     element: <Team />,
+    //     loader: teamLoader,
+    //   },
+    // ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <RouterProvider router={router} />
+      <p>hello</p>
+     */}
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
