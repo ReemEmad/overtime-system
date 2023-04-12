bf0544b0e9c13956ca07123aebae649e69d15b8d
Reem Emad <reem.emad@integrant.com>
2023-04-13 01:38:37 +0200

    signout

--- src/pages/CandidateLanding.tsx
+++ src/pages/CandidateLanding.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { appRoutes } from "../data/constants/appRoutes";
 
 function CandidateLanding() {
-  return <div>CandidateLanding</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Button onClick={() => navigate(appRoutes.SIGNOUT)}>Signout</Button>
+      <div>CandidateLanding</div>
+    </>
+  );
 }
 
 export default CandidateLanding;
